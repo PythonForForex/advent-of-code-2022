118ce0cb4b89b963a1d03ba89056cc10f50ee5cd
PythonForForex <info@pythonforforex.com>
2022-12-01 16:12:35 +0700

    first commit

--- day1.py
+++ day1.py
@@ -1,4 +1,3 @@
-#%%
 from get_input import get_input
 
 
@@ -9,8 +8,5 @@ print(max([sum([int(b) for b in i.split()]) for i in raw_input.split('\n\n')]))
 
 ## Part 2
 
-
 all_call = [sum([int(b) for b in i.split()]) for i in raw_input.split('\n\n')]
 print(sum(sorted(all_call)[-3:]))
-
-# %%
