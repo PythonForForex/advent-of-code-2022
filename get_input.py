5de876341ce13b8a064b36b5622d53dcb9861600
PythonForForex <info@pythonforforex.com>
2022-12-02 14:45:11 +0700

    auto dl inputs

---
+++ get_input.py
@@ -0,0 +1,46 @@
+import sqlite3
+from pathlib import Path
+from sqlite3.dbapi2 import OperationalError
+
+import requests
+
+cookie_jar = {}
+
+
+def get_cookie():
+	if cookie_jar:
+		return cookie_jar
+
+	try:
+		# db = f'{Path.home()}/.mozilla/firefox/v69ttedg.default-release/cookies.sqlite'
+		db = f'{Path.home()}/.mozilla/firefox/gape0v2q.default-release-1659362209175/cookies.sqlite'
+		con = sqlite3.connect(db, timeout=3)	
+		cursor = con.cursor()
+		query_cookies = cursor.execute('SELECT * from moz_cookies')
+		get_cookies = query_cookies.fetchall()
+
+		for line in get_cookies:
+			if '.adventofcode.com' in line:
+				cookie_jar[line[2]]=line[3]
+		return cookie_jar
+	except OperationalError:
+		return False
+
+
+def get_input(day=1):
+	cookie_jar = get_cookie()
+	if not cookie_jar:
+		try:
+			with open(f'inputs/day{day}.txt', 'r') as e:
+				raw_input = e.read()
+			return raw_input
+		except:
+			raise OperationalError('Input file unavailable')
+	
+	r = requests.get(f'https://adventofcode.com/2022/day/{day}/input', cookies=cookie_jar)
+	r.raise_for_status()
+
+	with open(f'inputs/day{day}.txt', 'w') as e:
+		e.write(r.text)
+
+	return r.text
